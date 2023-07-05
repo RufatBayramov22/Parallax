3fd0cb35c298257bcc147c274f3beade46f9790d
RufatBayramov22 <ibrahimovrufka@icloud.com>
2023-07-06 00:50:29 +0400

    first commit

---
+++ src/parallax/parallax-1/Parallax.js
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+import "../parallax-1/main.css"
+
+import React from "react";
+
+const Parallax = () => {
+  const [scrollPositon, setScrollPosition] = useState(0);
+
+  const handleScroll = () => setScrollPosition(window.pageYOffset);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <section
+        className="parallax"
+        style={{
+          backgroundSize: `${(window.outerHeight - scrollPositon) / 3}%`,
+        }}
+      >
+        <h2>Parallax</h2>
+        <button>Get Stared</button>
+      </section>
+      <section className="contanier">
+        <h2>What is Parallax?</h2>
+        <p>
+          Parallax is a displacement or difference in the apparent position of
+          an object viewed along two different lines of sight and is measured by
+          the angle or half-angle of inclination between those two lines. Due to
+          foreshortening, nearby objects show a larger parallax than farther
+          objects, so parallax can be used to determine distances.
+        </p>
+        <p>
+          Parallax is a displacement or difference in the apparent position of
+          an object viewed along two different lines of sight and is measured by
+          the angle or half-angle of inclination between those two lines. Due to
+          foreshortening, nearby objects show a larger parallax than farther
+          objects, so parallax can be used to determine distances.
+        </p>
+        <p>
+          Parallax also affects optical instruments such as rifle scopes,
+          binoculars, microscopes, and twin-lens reflex cameras that view
+          objects from slightly different angles. Many animals, along with
+          humans, have two eyes with overlapping visual fields that use parallax
+          to gain depth perception; this process is known as stereopsis. In
+          computer vision the effect is used for computer stereo vision, and
+          there is a device called a parallax rangefinder that uses it to find
+          the range, and in some variations also altitude to a target.
+        </p>
+        <p>
+          Parallax is a displacement or difference in the apparent position of
+          an object viewed along two different lines of sight and is measured by
+          the angle or half-angle of inclination between those two lines. Due to
+          foreshortening, nearby objects show a larger parallax than farther
+          objects, so parallax can be used to determine distances.
+        </p>
+        <p>
+          Parallax is a displacement or difference in the apparent position of
+          an object viewed along two different lines of sight and is measured by
+          the angle or half-angle of inclination between those two lines. Due to
+          foreshortening, nearby objects show a larger parallax than farther
+          objects, so parallax can be used to determine distances.
+        </p>
+        <p>
+          Parallax also affects optical instruments such as rifle scopes,
+          binoculars, microscopes, and twin-lens reflex cameras that view
+          objects from slightly different angles. Many animals, along with
+          humans, have two eyes with overlapping visual fields that use parallax
+          to gain depth perception; this process is known as stereopsis. In
+          computer vision the effect is used for computer stereo vision, and
+          there is a device called a parallax rangefinder that uses it to find
+          the range, and in some variations also altitude to a target.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default Parallax;
